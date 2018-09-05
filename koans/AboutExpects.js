ac7706600fcfa64e3e07b12b37c732a9adcac7d7
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2018-09-04 19:01:06 -0700

    fixed bugs

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -7,28 +7,28 @@ describe("About Expects", function() {
 
   // To understand reality, we must compare our expectations against reality.
   it("should expect equality", function() { 
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = 2;
 	  var actualValue = 1 + 1;
 	
-	  expect(actualValue !== expectedValue).toBeTruthy();
+	  expect(actualValue === expectedValue).toBeTruthy();
   });  
 
   // Some ways of asserting equality are better than others.
   it("should assert equality a better way", function() { 
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = 2;
 	  var actualValue = 1 + 1;
 	
   // toEqual() compares using common sense equality.
-	  expect(actualValue).toEqual(actualValue);
+	  expect(actualValue).toEqual(expectedValue);
   });
 
   // Sometimes you need to be precise about what you "type".
   it("should assert equality with ===", function() { 
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = '2';
 	  var actualValue = (1 + 1).toString();
 	
   // toBe() will always use === to compare.
-	  expect(expectedValue).toBe(expectedValue);
+	  expect(actualValue).toBe(expectedValue);
   });  
 
   // Sometimes we will ask you to fill in the values.
